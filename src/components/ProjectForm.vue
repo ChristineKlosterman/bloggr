9ff086025db2e89ae125d5da195006dd57db485c
EllaMarcum <108292821+EllaMarcum@users.noreply.github.com>
2022-08-18 16:35:35 -0600

    innit

---
+++ src/components/ProjectForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+
+    <p>Create a project</p>
+    <input type="text" placeholder="title" v-model="editable.title" />
+    <input type="text" placeholder="cover image url" v-model="editable.coverImg" />
+    <button class="btn btn-primary">{{ editable.id ? 'Save' : 'Create' }}</button>
+
+
+  </form>
+</template>
+
+
